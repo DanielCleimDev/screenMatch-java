085ca88cc18ec03f05ed8ca7824868cad314bd3b
DanielCleim <danielsgsmg@gmail.com>
2026-03-03 13:19:47 -0300

    Enviando projeto

---
+++ src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
@@ -0,0 +1,17 @@
+package br.com.alura.screenmatch.service;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConverteDados implements IConverteDados {
+    public ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) {
+        try {
+            return mapper.readValue(json, classe);
+        } catch (JacksonException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
